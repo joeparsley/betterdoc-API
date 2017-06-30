2c04259f957be29935639c59c6af1c06ee9e77ea
Joe Parsley <voodoojoenut@gmail.com>
2017-06-30 16:48:22 -0700

    final commit before project submit, unfished at this point

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -18,17 +18,20 @@ $('#form').submit(function(e) {
   var symptomSplit = userSymptom.replace(/[,]\s/g, '%2C%20').replace(/\s/g, '%20');
   var addressSplit = userAddress.replace(/\s/g, '+');
 
-
   var userFinal = new UserInfo(symptomSplit, addressSplit);
-  userFinal.getSymptom();
+
   userFinal.getLocation();
+
+
   console.log(userFinal);
 
+  // displayResponse(.results[0].geometry.location.lat);
 
 
 
 
-});
+
+  });
 
 
 });
--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ var apiKeyMap = require('./../.env').apiKeyMap;
 
 var UserInfo = function(symptom, address) {
   this.symptom = symptom;
-  this.address = address;
+  this.address = [];
 };
 
 UserInfo.prototype.getSymptom = function() {
@@ -19,7 +19,10 @@ UserInfo.prototype.getSymptom = function() {
 
 UserInfo.prototype.getLocation = function() {
   $.get('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=' + apiKeyMap).then(function(response){
-    console.log(response);
+    // var lat = response.results[0].geometry.location.lat;
+    // var lng = response.results[0].geometry.location.lng;
+    console.log(response.results[0].geometry.location.lat);
+    console.log(response.results[0].geometry.location.lng);
   })
     .fail(function(error) {
     $('#output').text("try again");
