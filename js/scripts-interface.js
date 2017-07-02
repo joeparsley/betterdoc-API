42646a89340716b5ef8df1972897e557ec7eb96d
Joe Parsley <voodoojoenut@gmail.com>
2017-07-02 10:04:54 -0700

    clean up js files, add notes, about to start on front end styling

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,38 +1,28 @@
+// REQUIRE CONSTRUCTOR FROM BACK-END
 var UserInfo = require('./../js/scripts.js').userInfoModule;
-
+// DISPLAY FUNCTION
 var displayResponse = function(response) {
   $('.output').text(response);
 }
-
+// START DOCUMENT READY
 $(document).ready(function() {
-
+ // START SUBMIT FUNCTION
 $('#form').submit(function(e) {
   e.preventDefault();
-
+  // STORE USER DATA
   var userSymptom = $('#user-symptom').val();
   var userAddress = $('#user-address').val();
-
+  // FORMAT USERINPUT FOR GET REQUEST
   var symptomSplit = userSymptom.replace(/[,]\s/g, '%2C%20').replace(/\s/g, '%20');
   var addressSplit = userAddress.replace(/\s/g, '+');
-
+  // INITIALIZE NEW OBJECT
   var userFinal = new UserInfo(symptomSplit, addressSplit);
-
+  // RUN INFORMATION THROUGH PROTOTYPES IN BACK-END
   userFinal.getLocation( (lat,lng) => {
     userFinal.getSymptom(displayResponse);
   });
-
-
-
-
   console.log(userFinal);
-
-  // displayResponse(.results[0].geometry.location.lat);
-
-
-
-
-
   });
-
+// END SUBMIT FUNCTION
 
 });
--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
+// API KEYS
 var apiKey = require('./../.env').apiKey;
 var apiKeyMap = require('./../.env').apiKeyMap;
-
+// CONSTUCTOR
 var UserInfo = function(symptom, address) {
   this.symptom = symptom;
   this.address = address;
   this.lat = "";
   this.lng = "";
 };
-
-var user;
+// PROTOTYPE FOR BETTERDOC API GET REQUEST
 UserInfo.prototype.getSymptom = function(displayResponse) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + this.symptom + '&location=' + this.lat + '%2C' + this.lng + '%2C100&user_location=' + this.lat + '%2C'+ this.lng + '&skip=0&limit=1&user_key=' + apiKey).then(function(response){
     console.log(response);
@@ -19,8 +19,7 @@ UserInfo.prototype.getSymptom = function(displayResponse) {
   });
 
 };
-
-
+// PROTOTYPE FOR GOOGLEMAPS API GET REQUEST
 UserInfo.prototype.getLocation = function(cb) {
   var self = this;
   $.get('https://maps.googleapis.com/maps/api/geocode/json?address=;' + this.address + '+CA&key=' + apiKeyMap).then(function(response) {
@@ -34,10 +33,5 @@ UserInfo.prototype.getLocation = function(cb) {
     $('#output').text("Ooops!");
   });
 };
-
-UserInfo.prototype.test= function()  {
-
-}
-
-
+// EXPORT TO INTERFACE
 exports.userInfoModule = UserInfo;
